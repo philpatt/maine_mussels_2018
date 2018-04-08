80648c5620046799649aaedb17945efbf681a8aa
philpatt <phillip.s.patterson1@gmail.com>
2018-04-08 14:21:12 -0700

    added tumblr media connection

--- wp-content/themes/mainemussels/functions.php
+++ wp-content/themes/mainemussels/functions.php
@@ -1,24 +1,26 @@
 <?php
 
-    function mainemussels_script_enqueue() {
-        wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/mainemussels.css', array(), '1.0.0', 'all' );
-        wp_enqueue_script( 'customjs', get_stylesheet_directory_uri() . '/js/mainemussels.js', array(), '1.0.0', 'true' );
-        
-    }
+function mainemussels_script_enqueue() {
+    wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/mainemussels.css', array(), '1.0.0', 'all' );
+    wp_enqueue_script( 'customjs', get_stylesheet_directory_uri() . '/js/mainemussels.js', array(), '1.0.0', 'true' );
+    
+}
 
-    add_action( 'wp_enqueue_scripts', 'mainemussels_script_enqueue' );
+add_action( 'wp_enqueue_scripts', 'mainemussels_script_enqueue' );
 
 
 
-    function mainemussels_theme_setup() {
-        
-        add_theme_support('menus');
-        
-        register_nav_menu('primary', 'Primary Header Navigation');
-        register_nav_menu('secondary', 'Footer Navigation');
-        
-    }
+function mainemussels_theme_setup() {
+    
+    add_theme_support('menus');
+    
+    register_nav_menu('primary', 'Primary Header Navigation');
+    register_nav_menu('secondary', 'Footer Navigation');
+    
+}
 
-    add_action('init', 'mainemussels_theme_setup');
+add_action('init', 'mainemussels_theme_setup');
 
-?>
+
+add_theme_support('custom-background');
+add_theme_support('custom-header');
--- wp-content/themes/mainemussels/page-131.php
+++ wp-content/themes/mainemussels/page-131.php
@@ -1,20 +1,5 @@
 <!-- Home page -->
 <?php get_header(); ?>
-
-<?php
-    if( have_posts() ):
-        while( have_posts() ): the_post(); ?>
-            <h3><?php the_title(); ?></h3>        
-            <p><?php the_content(); ?><p>
-            <p><?php the_comment(); ?><p>
-            
-
-
-            <hr>
-
-        <?php endwhile;
-        
-    endif;
-?>
-<h2>Home page</h2>
+<h2>Welcome!</h2>
+<img src="<?php header_image(); ?>" alt="">
 <?php get_footer(); ?>
