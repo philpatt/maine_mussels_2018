2350b2b2a0db65152a6a81c78a4143e9dab07da1
philpatt <phillip.s.patterson1@gmail.com>
2018-04-08 18:50:40 -0700

    setting up navbar

--- wp-content/themes/mainemussels/functions.php
+++ wp-content/themes/mainemussels/functions.php
@@ -1,5 +1,19 @@
 <?php
 
+/*
+    ====================
+    include scripts
+    ====================
+*/
+
+if ( ! file_exists( get_template_directory() . '/class-wp-bootstrap-navwalker.php' ) ) {
+	// file does not exist... return an error.
+	return new WP_Error( 'class-wp-bootstrap-navwalker-missing', __( 'It appears the class-wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+} else {
+	// file exists... require it.
+    require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
+}
+
 function mainemussels_script_enqueue() {
     wp_enqueue_style( 'customstyle', get_template_directory_uri() . '/css/mainemussels.css', array(), '1.0.0', 'all' );
     wp_enqueue_script( 'customjs', get_stylesheet_directory_uri() . '/js/mainemussels.js', array(), '1.0.0', 'true' );
@@ -9,18 +23,32 @@ function mainemussels_script_enqueue() {
 add_action( 'wp_enqueue_scripts', 'mainemussels_script_enqueue' );
 
 
+/*
+    ====================
+    Activate menus
+    ====================
+*/
+
+register_nav_menus( array(
+	'primary' => __( 'Primary Menu', 'mainemussels' ),
+) );
+
 
 function mainemussels_theme_setup() {
-    
+
     add_theme_support('menus');
     
-    register_nav_menu('primary', 'Primary Header Navigation');
+    register_nav_menu('primary', 'Header Navigation');
     register_nav_menu('secondary', 'Footer Navigation');
     
 }
-
 add_action('init', 'mainemussels_theme_setup');
 
+/*
+    ====================
+    Theme Support
+    ====================
+*/
 
 add_theme_support('custom-background');
 add_theme_support('custom-header');
--- wp-content/themes/mainemussels/header.php
+++ wp-content/themes/mainemussels/header.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Maine Mussels</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <?php wp_head(); ?>
     </head>
     <?php
@@ -16,7 +17,27 @@
 
     ?>
     <body <?php body_class( $mainemussels_classes );?>>
-        <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
+       <nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand" href="#">Maine Mussels</a>
+                <?php
+                wp_nav_menu( array(
+                    'theme_location'    => 'primary',
+                    'depth'             => 0,
+                    'container'         => 'div',
+                    'container_class'   => 'collapse navbar-collapse',
+                    'container_id'      => 'bs-example-navbar-collapse-1',
+                    'menu_class'        => 'nav navbar-nav',
+                    'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                    'walker'            => new WP_Bootstrap_Navwalker()
+                ) );
+                ?>
+            </div>
+        </nav>
         <img src="<?php header_image(); ?>" alt="">
 
         
